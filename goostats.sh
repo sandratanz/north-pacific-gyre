189df6d29cb0c394e9084067ead3dfb90806a40e
Sandra <sandra.k.tanz@gmail.com>
2024-09-06 16:08:45 +0800

    Compute the range of values

--- goostats.sh
+++ goostats.sh
@@ -7,5 +7,5 @@ echo "Welcome to Nell's stats script"
 # Compute the min/max/range of values in a file.
 min=$( cat ${fname} | sort | head -1)
 max=$( cat $(fname) | sort \ tail -1 )
-
+range=$( echo "${max}-${min}" | bc -l)
 
